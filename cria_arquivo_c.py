7568f075c10a024bdc1ad56e0cb9ef78191871ff
RodolfoHRE <64434994+RodolfoHRE@users.noreply.github.com>
2023-11-11 12:17:18 -0300

    código modificado para abranger todas as possibilidades de tradução

--- cria_arquivo_c.py
+++ cria_arquivo_c.py
@@ -1,23 +1,36 @@
+ 
+## função referente a criação de arquivo.c apartir das leituras feitas pelo
+## interpretador sintatico e semantico do visualAlg
 
-def add_arquivo_c(resultado_sintático, var, valor, ind):
+def add_arquivo_c(resultado_sintático, var, valor, ind=0, operacao="+", condicao="i<10"):
 
     nova_linha = [
+        #-----padroes-----
         "#include <stdio.h>\n",
-        "#define True 1\n",
-        "#define False 0\n",
         "int main() {\n",
         "  return 0;}\n",
+        #-----definições-----
+        "#define True 1\n",
+        "#define False 0\n",
+        #-----variaveis------
         "float "+ var+";\n",
         "float "+ var+"="+str(valor)+";\n",
         "char" + var+"["+ind+"];\n",
         "char" + var+"[]"+ "'"+valor+"';\n",
         "int "+ var+";\n",                      #bool
-        "int "+ var+"= True ;\n",
-        "int "+ var+"= False ;\n",
-        "",
-        "",
-        "",
-        "",
+        "int "+ var+"= True ;\n",               #bool
+        "int "+ var+"= False ;\n",              #bool
+        #------operações--------
+         operacao+ var,
+        operacao+ valor,
+        "(",
+        ")",
+        #------funcionalidades-------
+        "if("+ +"){",
+        "else{",
+        "while("+ condicao +"){",
+        "for("+  +";"+ condicao +";"+  +"){",
+        "}"
         
     ]
     nome_arquivo = "arquivo.c"
@@ -30,7 +43,7 @@ def add_arquivo_c(resultado_sintático, var, valor, ind):
 res=["12345"]
 var="a"
 valor = "2"
-ind= "0"
 
-add_arquivo_c(res, var, valor, ind)
+
+add_arquivo_c(res, var, valor)
 
