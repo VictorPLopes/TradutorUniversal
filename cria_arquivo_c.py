01ca99658e8e9fb53edc04df5b7793c78b6ec7e8
RodolfoHRE <64434994+RodolfoHRE@users.noreply.github.com>
2023-11-11 17:07:24 -0300

    erro de tradução dupla resulvido, e automatização da tradução do código c sem necessidade de apagar o arquivo

--- cria_arquivo_c.py
+++ cria_arquivo_c.py
@@ -10,8 +10,8 @@ def add_linha(i, var="i", valor="0", ind="0", operacao="+", condicao="i<10"):
         "int main() {\n",                               #id1
         "  return 0;}\n",                               #id2
         #-----definições-----
-        "#define True 1\n",                             #id3
-        "#define False 0\n",                            #id4
+        "#define True 1",                             #id3
+        "#define False 0",                            #id4
         #-----variaveis------
         "float "+ var+";\n",                             #id5
         "float "+ var+"="+ valor+";\n",                  #id6
@@ -30,7 +30,7 @@ def add_linha(i, var="i", valor="0", ind="0", operacao="+", condicao="i<10"):
         "else{\n",                                      #id17
         "while("+ condicao +"){\n",                     #id18
         "}\n",                                          #id19
-        "\n",                                           #id20
+        "",                                             #id20
     ]
 
     return nova_linha[i]
@@ -42,30 +42,58 @@ def add_linha(i, var="i", valor="0", ind="0", operacao="+", condicao="i<10"):
 
 def add_arquivo_c(resultado_sintatico):
 
-    nome_arquivo = "arquivo.c"
 
     for posicao in range(len(resultado_sintatico)):
 
         linha = []
 
-        if resultado_sintatico[posicao][1] == 'algoritmo':
+        if resultado_sintatico[posicao][0] == 'algoritmo':
 
             linha.append(add_linha(0))
             linha.append(add_linha(3))
             linha.append(add_linha(4))
-            linha.append(add_linha(1))
-            linha.append(add_linha(2))
+            linha.append(add_linha(20))
 
             for i in range(len(linha)):
                 with open(nome_arquivo, "a") as arquivo:
                     arquivo.write(linha[i]+"\n")
 
-        else:
-            print("resultado desconhecido")
+        elif resultado_sintatico[posicao][0] == 'var':
 
-res=[["algoritmo"],["algoritmo"],
-     ["inicio"],["inicio"]
-     ]
+           
+
+            for i in range(len(linha)):
+                with open(nome_arquivo, "a") as arquivo:
+                    arquivo.write(linha[i]+"\n")
+
+        elif resultado_sintatico[posicao][0] == 'inicio':
+
+            linha.append(add_linha(1))
+
+            for i in range(len(linha)):
+                with open(nome_arquivo, "a") as arquivo:
+                    arquivo.write(linha[i]+"\n")
+
+
+        elif resultado_sintatico[posicao][0] == 'fimalgoritmo':
+
+            linha.append(add_linha(2)) 
+
+            for i in range(len(linha)):
+                with open(nome_arquivo, "a") as arquivo:
+                    arquivo.write(linha[i]+"\n")
+
+nome_arquivo = "arquivo.c"
+
+with open(nome_arquivo, "w") as arquivo:
+    arquivo.write("")
+
+res=[
+    ['algoritmo', 'algoritmo'],
+    ['var', 'var'],
+    ['inicio', 'inicio'],
+    ['fimalgoritmo', 'fimalgoritmo']
+    ]
 var="a"
 valor = "2"
 
