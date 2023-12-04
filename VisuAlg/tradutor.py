abf644f278026cecd01488c466c162bf44d5c738
VictorPLopes <victor.probio@gmail.com>
2023-12-03 21:41:08 -0300

    Teste de um tradutor alternativo

--- VisuAlg/tradutor.py
+++ VisuAlg/tradutor.py
@@ -1,357 +1,163 @@
-VERSAO= "2.0.4"
+em_leia = False # variavel que indica se esta dentro de um leia
+em_escreva = False # variavel que indica se esta dentro de um escreva
+em_para = False # variavel que indica se esta dentro de um para
+em_var = False # variavel que indica se esta dentro de um var
+em_atrib = False # variavel que indica se esta dentro de um atribuicao
 
-##----flags-----
-WT = 0
-RD = 0
+nivel_indentacao = 0 # variavel que indica o nivel de indentacao atual
 
+para_atual = [] # lista que armazena os dados do para atual
 
+# Dicionário para tradução de casos onde a tradução é direta, ou seja, a estrutura é a mesma em C e em VisuAlg
+dicionario_traducao_direta = {
+    "algoritmo":"""
+#include <stdio.h>
 
-##função aulixar referente a tradução para a linguagem C
+#define TRUE 1
+#define FALSE 0
 
-def add_linha(i, var="i",var_2 = "y", valor="0", ind="100", operacao="+", condicao="i < 10"):
+""",
+    "inicio": "int main() {\n",
+    "=": "=",
+    "(":"(",
+    ")":")",
+    ">":">",
+    "<":"<",
+    ">=":">=",
+    "<=":"<=",
+    "==":"==",
+    "<>":"!=",
+    "se":"if",
+    "entao": "{\n",
+    "senao": "} else {\n",
+    "fimse": "}",
+    "verdadeiro": "TRUE",
+    "falso": "FALSE",
+    "faca": "{\n",
+    "fimpara": "}",
+    "enquanto": "while",
+    "fimenquanto": "}",
+    "+": "+",
+    "-": "-",
+    "*": "*",
+    "/": "/",
+    "e": "&&",
+    "ou": "||",
+    "fimalgoritmo": "return 0;\n}",
+    "inteiro": "int",
+    "real": "float",
+    "logico": "int"
+}
 
-    nova_linha = [
-    #-----padroes-----
-    "#include <stdio.h>\n",            # id0
-    "int main() {\n",                  # id1
-    "  return 0;\n",                   # id2
-    #-----definicoes-----
-    "#define True 1\n",                # id3
-    "#define False 0\n",               # id4
-     #-----variaveis------
-    "int " + var + ";\n",              # id5
-    "float " + var + ";\n",            # id6
-    "char " + var + "[" + ind + "];\n",# id7
-    #------funcionalidades-------
-    'scanf"',                          # id8
-    '%d", &',                          # id9
-    '%f", &',                          # id10
-    '%s", ',                           # id11
-    '%c", &',                          # id12
-    '", &',                            # id13
-    'printf',                          # id14
-    ','+ var + ');\n',                 # id15
-    'if ',                            # id16
-    '} else {',                        # id17
-    'while',                           # id18
-    'for (',                           # id19
-    '; '+ var + '++) {\n',             # id20
-    '{\n',                             # id21
-    '}\n',                             # id22
-    ';\n',                             # id23
-    ';'                                # id24
-]
+dicionario_scanf_printf = {
+    "inteiro": f"%d",
+    "real": f"%f",
+    "caractere": f"%s"
+}
 
+def get_indentacao():
+    global nivel_indentacao
+    return "    " * nivel_indentacao
 
-
-
-    return nova_linha[i]
-
-## Lista de condicionais
-
-def condicional(i, var='a', valor="5", express = " x + 1"):
-    cond=[
-    var + ">" + var,    #id0
-    var + ">" + valor,  #id1
-    valor + ">" + var,  #id2
-    var + "<" + var,    #id3
-    var + "<" + valor,  #id4
-    valor + "<" + var,  #id5
-    var + ">=" + var,   #id6
-    var + ">=" + valor, #id7
-    valor + ">=" + var, #id8
-    var + "<=" + var,   #id9
-    var + "<=" + valor, #id10
-    valor + "<=" + var, #id11
-    var + "==" + var,   #id12
-    var + "==" + valor, #id13
-    valor + "==" + var, #id14
-    var + "!=" + var,   #id15
-    var + "!=" + valor, #id16
-    valor + "!=" + var, #id17
-    "&&",               #id18
-    "||"                #id19
-]
+# Função que traduz o código de VisuAlg para C
+def tradutor(tabela_classificacao, tabela_variavies):
+    # Variáveis globais
+    global em_leia, em_escreva, em_para, em_var, em_caractere, em_atrib, nivel_indentacao
     
-    return cond[i]
-
-## função referente a criação de template.c apartir das leituras feitas pelo
-## interpretador sintatico e semantico do visualAlg
-
-def add_arquivo_c(resultado_sintatico):
+    # Variável que armazena o código traduzido
+    codigo = ""
     
-    for posicao in range(len(resultado_sintatico)):
-
-        linha = []
-
-        if resultado_sintatico[posicao][0] == 'algoritmo':
-
-            linha.append(add_linha(0))
-            linha.append(add_linha(3))
-            linha.append(add_linha(4))
-            
-
-            for i in range(len(linha)):
-                with open(nome_arquivo, "a") as arquivo:
-                    arquivo.write(linha[i]+"\n")
-
-        elif resultado_sintatico[posicao][0] == 'var':
-
-            if resultado_sintatico[posicao][1] == 'var':
-
-                while(resultado_sintatico[posicao+1][0] != 'inicio'):
-                    if resultado_sintatico[posicao][0]== "inteiro" or resultado_sintatico[posicao][0]== "logico":
-                        linha.append(add_linha(5,resultado_sintatico[posicao][1]))
-                    elif resultado_sintatico[posicao][0]== "real":
-                        linha.append(add_linha(6,resultado_sintatico[posicao][1]))
-                    elif resultado_sintatico[posicao][0]== "caracter":
-                        linha.append(add_linha(7,resultado_sintatico[posicao][1]))
-
-                    prosicao=posicao+1
-
-            for i in range(len(linha)):
-                with open(nome_arquivo, "a") as arquivo:
-                    arquivo.write(linha[i]+"\n")
-
-        elif resultado_sintatico[posicao][0] == 'inicio':
-
-            linha.append(add_linha(1))
-
-            for i in range(len(linha)):
-                with open(nome_arquivo, "a") as arquivo:
-                    arquivo.write(linha[i]+"\n")
-
-        elif resultado_sintatico[posicao][0] == 'leia':
-
-            RD = 1
-
-            linha.append(add_linha(8))
-
-            for i in range(len(linha)):
-                with open(nome_arquivo, "a") as arquivo:
-                    arquivo.write(linha[i]+"\n")
-
-        elif resultado_sintatico[posicao][0] == 'escreve':
-
-            WT = 1
-
-            linha.append(add_linha(14)) 
-
-            for i in range(len(linha)):
-                with open(nome_arquivo, "a") as arquivo:
-                    arquivo.write(linha[i]+"\n")
-
-        elif resultado_sintatico[posicao][0] == 'se':
-
-            linha.append(add_linha(16))  
-
-            for i in range(len(linha)):
-                with open(nome_arquivo, "a") as arquivo:
-                    arquivo.write(linha[i]+"\n")
-            
-        elif resultado_sintatico[posicao][0] == 'senao':
-
-            linha.append(add_linha(17))   
-
-            for i in range(len(linha)):
-                with open(nome_arquivo, "a") as arquivo:
-                    arquivo.write(linha[i]+"\n")
-
-        elif resultado_sintatico[posicao][0] == 'entao' or resultado_sintatico[posicao][0] == 'faca':
-
-            linha.append(add_linha(21))     
-            for i in range(len(linha)):
-                with open(nome_arquivo, "a") as arquivo:
-                    arquivo.write(linha[i]+"\n")
-
-        elif resultado_sintatico[posicao][0] == 'enquanto':
-
-            linha.append(add_linha(18))    
-
-            for i in range(len(linha)):
-                with open(nome_arquivo, "a") as arquivo:
-                    arquivo.write(linha[i]+"\n")
-            
-        elif resultado_sintatico[posicao][0] == 'para':
-
-            linha.append(add_linha(19))    
-            posicao = posicao + 1
-            while(resultado_sintatico[posicao+1][0] != 'inicio'):
-                
-                if resultado_sintatico[posicao][1]== "var":
-                    linha.append(resultado_sintatico[posicao][0])
-                elif resultado_sintatico[posicao][0]== "de":
-                    linha.append("=")
-                elif resultado_sintatico[posicao][1]== "valor":
-                    linha.append(resultado_sintatico[posicao][0])
-                elif resultado_sintatico[posicao][0]== "ate":
-                    linha.append(";" + resultado_sintatico[posicao-3][0] + "<")
-                elif resultado_sintatico[posicao][0]== "faca":
-                    linha.append(";" + resultado_sintatico[posicao-5][0] + "++ ) {")
-
-                posicao = posicao + 1
-
-            for i in range(len(linha)):
-                with open(nome_arquivo, "a") as arquivo:
-                    arquivo.write(linha[i]+"\n")
+    # Para cada palavra na tabela de classificação
+    for palavra in tabela_classificacao:
+        # Se estiver na parte de declaração de variáveis
+        if em_var:
+            # Se chegar no fim da declaração de variáveis
+            if palavra[0] == "inicio":
+                em_var = False
+                codigo += dicionario_traducao_direta[palavra[0]]
+                nivel_indentacao += 1
+                continue
+            # Se estiver declarando uma variável
+            if palavra[1] == "var":
+                # Se for do tipo caractere
+                if tabela_variavies[palavra[0]] == "caractere":
+                    codigo += "char " + palavra[0] + "[50];\n"
+                    continue
+                # Se for do tipo inteiro, real ou lógico
+                codigo += tabela_variavies[palavra[0]] + palavra[0] + ";\n"
+                continue
         
-        elif resultado_sintatico[posicao][0] == 'fimenquanto' or resultado_sintatico[posicao][0] == 'fimpara' or resultado_sintatico[posicao][0] == 'fimse':
-            
-            linha.append(add_linha(22))
-
-            for i in range(len(linha)):
-                with open(nome_arquivo, "a") as arquivo:
-                    arquivo.write(linha[i]+"\n")
-
-        elif resultado_sintatico[posicao][0] == 'fimalgoritmo':
-
-            linha.append(add_linha(2)) 
-
-            for i in range(len(linha)):
-                with open(nome_arquivo, "a") as arquivo:
-                    arquivo.write(linha[i]+"\n")
-
-        elif resultado_sintatico[posicao][0] == '(':
-
-            linha.append("(")    
-
-            for i in range(len(linha)):
-                with open(nome_arquivo, "a") as arquivo:
-                    arquivo.write(linha[i]+"\n")
-
-        elif resultado_sintatico[posicao][0] == ')':
-
-            linha.append(")")    
-
-            for i in range(len(linha)):
-                with open(nome_arquivo, "a") as arquivo:
-                    arquivo.write(linha[i]+"\n")
-
-        elif resultado_sintatico[posicao][0] == '>':
-
-            linha.append(">")    
-
-            for i in range(len(linha)):
-                with open(nome_arquivo, "a") as arquivo:
-                    arquivo.write(linha[i]+"\n")
-
-        elif resultado_sintatico[posicao][0] == '<':
-
-            linha.append("<")    
-
-            for i in range(len(linha)):
-                with open(nome_arquivo, "a") as arquivo:
-                    arquivo.write(linha[i]+"\n")
-
-        elif resultado_sintatico[posicao][0] == '==':
-
-            linha.append("==")    
-
-            for i in range(len(linha)):
-                with open(nome_arquivo, "a") as arquivo:
-                    arquivo.write(linha[i]+"\n")
-
-        elif resultado_sintatico[posicao][0] == '=':
-
-            linha.append("=")    
-
-            for i in range(len(linha)):
-                with open(nome_arquivo, "a") as arquivo:
-                    arquivo.write(linha[i]+"\n")
-
-        elif resultado_sintatico[posicao][0] == '<=':
-
-            linha.append("<=")    
-
-            for i in range(len(linha)):
-                with open(nome_arquivo, "a") as arquivo:
-                    arquivo.write(linha[i]+"\n")
-
-        elif resultado_sintatico[posicao][0] == '>=':
-
-            linha.append(">=")    
-
-            for i in range(len(linha)):
-                with open(nome_arquivo, "a") as arquivo:
-                    arquivo.write(linha[i]+"\n")
-
-        elif resultado_sintatico[posicao][0] == '<>':
-
-            linha.append("!=")    
-
-            for i in range(len(linha)):
-                with open(nome_arquivo, "a") as arquivo:
-                    arquivo.write(linha[i]+"\n")
-
-        elif resultado_sintatico[posicao][0] == '+':
-
-            linha.append("+")    
-
-            for i in range(len(linha)):
-                with open(nome_arquivo, "a") as arquivo:
-                    arquivo.write(linha[i]+"\n")
-
-        elif resultado_sintatico[posicao][0] == '-':
-
-            linha.append("-")    
-
-            for i in range(len(linha)):
-                with open(nome_arquivo, "a") as arquivo:
-                    arquivo.write(linha[i]+"\n")
-
-        elif resultado_sintatico[posicao][0] == '*':
-
-            linha.append("*")    
-
-            for i in range(len(linha)):
-                with open(nome_arquivo, "a") as arquivo:
-                    arquivo.write(linha[i]+"\n")
-
-        elif resultado_sintatico[posicao][0] == '/':
-
-            linha.append("/")    
-
-            for i in range(len(linha)):
-                with open(nome_arquivo, "a") as arquivo:
-                    arquivo.write(linha[i]+"\n")
-
-        elif resultado_sintatico[posicao][0] == 'e':
-
-            linha.append("&&")    
-
-            for i in range(len(linha)):
-                with open(nome_arquivo, "a") as arquivo:
-                    arquivo.write(linha[i]+"\n")
+        # Se for uma palavra de tradução direta
+        if palavra[0] in dicionario_traducao_direta:
+            # Checa se for atribuição
+            if palavra[0] == "=":
+                em_atrib = True
+            # Checa se está no faça de um para
+            if palavra[0] == "faca":
+                nivel_indentacao += 1
+                codigo += para_atual[0] + " = " + para_atual[1] + "; " + para_atual[0] + " <= " + para_atual[2] + "; " + para_atual[0] + "++)"
+                # Limpa a lista do para atual
+                para_atual.clear()
+            # Checa se for um se, enquanto ou senão
+            if palavra[0] == "se" or palavra[0] == "enquanto" or palavra[0] == "senao":
+                nivel_indentacao += 1
+            # Checa se for um fimse, fimenquanto, fimpara ou fimalgoritmo
+            if palavra[0] == "fimse" or palavra[0] == "fimenquanto" or palavra[0] == "fimpara" or palavra[0] == "fimalgoritmo":
+                nivel_indentacao -= 1
+            codigo += dicionario_traducao_direta[palavra[0]]
+            # Checa se está em atribuição
+            if em_atrib and palavra[0] == ")":
+                codigo += ";\n"
+                em_atrib = False
+            continue
         
-        elif resultado_sintatico[posicao][0] == 'ou':
-
-            linha.append("||")    
-
-            for i in range(len(linha)):
-                with open(nome_arquivo, "a") as arquivo:
-                    arquivo.write(linha[i]+"\n")
-
-        elif resultado_sintatico[posicao][0] == 'msg':
-
-            linha.append(resultado_sintatico[posicao][0] + ";")    
-
-            for i in range(len(linha)):
-                with open(nome_arquivo, "a") as arquivo:
-                    arquivo.write(linha[i]+"\n")
-
-nome_arquivo = "template.c"
-
-with open(nome_arquivo, "w") as arquivo:
-    arquivo.write("")
-
-res=[
-    ['algoritmo', 'algoritmo'],
-    ['var', 'var'],
-    ['inicio', 'inicio'],
-    ['fimalgoritmo', 'fimalgoritmo']
-    ]
-var="a"
-valor = "2"
-
-
-add_arquivo_c(res)
+        # Senão, se for a parte de declaração de variáveis
+        if palavra[0] == "var":
+            em_var = True
+            continue
+        # Senão, se for o nome de uma variável
+        if palavra[1] == "var":
+            # Se estiver em um leia
+            if em_leia:
+                codigo += dicionario_scanf_printf[tabela_variavies[palavra[0]]] + ", &" + palavra[0] + ");\n"
+                em_leia = False
+                continue
+            # Se estiver em um escreva
+            if em_escreva:
+                codigo += dicionario_scanf_printf[tabela_variavies[palavra[0]]] + ", " + palavra[0] + ");\n"
+                em_escreva = False
+                continue
+            # Se estiver em um para
+            if em_para:
+                para_atual.append(palavra[0])
+                continue
+            codigo += palavra[0]
+            continue
+        # Senão, se for uma mensagem (string)
+        if palavra[1] == "msg":
+            codigo += palavra[0]
+            continue
+        # Senão, se for um leia
+        if palavra[0] == "leia":
+            em_leia = True
+            codigo += get_indentacao() + "scanf("
+            continue
+        # Senão, se for um escreva
+        if palavra[0] == "escreva":
+            em_escreva = True
+            codigo += get_indentacao() + "printf("
+            continue
+        # Senão, se for um para
+        if palavra[0] == "para":
+            em_para = True
+            codigo += get_indentacao() + "for("
+            continue
+        # Senão, se for um valor inteiro
+        if palavra[1] == "inteiro":
+            # Se estiver em um para
+            if em_para:
+                para_atual.append(palavra[0])
+                continue
+    
+    # Retorna o código traduzido
+    return codigo
